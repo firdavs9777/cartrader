a232a5b3262378332dda8857ac75e5f0563d4d37
firdavs9777 <fmutalipov97@gmail.com>
2023-04-11 17:46:30 +0900

    First Commit

--- components/car/Card.vue
+++ components/car/Card.vue
@@ -3,13 +3,14 @@ import heartFilled from '@/assets/heartFilled.png'
 import heartOutline from '@/assets/heartOutline.png'
 // Using props in Nuxt
 const props = defineProps({
-  car: Object
+  car: Object,
+  favored: Boolean
 })
-const favored = useState(`favored-${props.car.id}`, () => {
-  console.log(`${props.car.id}`)
-  return false
-})
-
+// const favored = useState(`favored-${props.car.id}`, () => {
+//   return false
+// })
+// Emitting function from parent 
+const emit = defineEmits(['favor'])
 </script>
 <template>
 	<div
@@ -17,7 +18,7 @@ const favored = useState(`favored-${props.car.id}`, () => {
         >
         <div class="flex h-full" >
          <!-- Change like and dislike button dynamically using click button -->
-          <img  class="absolute w-7 right-5 top-2 z-20" :src="favored? heartFilled: heartOutline" alt="favorite" @click="favored = !favored"/>
+          <img  class="absolute w-7 right-5 top-2 z-20" :src="favored? heartFilled: heartOutline" alt="favorite" @click="emit('favor',car.id)"/>
           <img
             :src="car.url"
             alt=""
@@ -34,7 +35,7 @@ const favored = useState(`favored-${props.car.id}`, () => {
             <h1 class="mt-auto text-xl">{{car.price}}</h1>
           </div>
         </div>
-		   <div class="flex h-full">
+		   <!-- <div class="flex h-full">
           <img
             src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
             alt=""
@@ -49,6 +50,6 @@ const favored = useState(`favored-${props.car.id}`, () => {
             </div>
             <h1 class="mt-auto text-xl">$39,555</h1>
           </div>
-        </div>
+        </div> -->
       </div>
 </template>
